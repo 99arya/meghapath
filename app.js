f9d64166e0c020ef33969e1c24d1f91529e837fc
Sumit <red.fyrd@gmail.com>
2018-02-09 11:41:25 +0000

    sendgrid remove

--- app.js
+++ app.js
@@ -20,25 +20,7 @@ var passport = require("passport"),
 
 var port = process.env.PORT || 80;
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('test@example.com');
-var to_email = new helper.Email('sumit.arya@channelplay.in');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'Hello, Email!');
-var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
-
-sg.API(request, function(error, response) {
-  console.log(response.statusCode);
-  console.log(response.body);
-  console.log(response.headers);
-});
   //=========================================================================================================================                                    
  //  PASSPORT CONFIGURATION
 //=========================================================================================================================                                    
@@ -64,7 +46,8 @@ app.use(bodyParser.json());
 
 
     // mongoose.connect("mongodb://localhost/cafe_o2")
-    mongoose.connect("mongodb://sumitarya:behaPPYhaha99!@ds255715.mlab.com:55715/o2db")
+    // mongoose.connect("mongodb://sumitarya:behaPPYhaha99!@ds255715.mlab.com:55715/o2db")
+    mongoose.connect(process.env.MONGOURL)
     // mongoose.Promise = require('bluebird');
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.static(__dirname + '/views'));
